ebddc85f4d111fa173e62c7d47bfd891c0e4970e
Kareemalaa <104287083+hhgffffffff@users.noreply.github.com>
2022-07-14 19:14:50 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,26 @@
+function openNav(){
+  document.getElementById("mysidenav") .style.width="450px";
+  document.getElementById("main") .style.width="450px";
+}
+
+function closeNav(){
+  document.getElementById("mysidenav") .style.width="0px";
+  document.getElementById("main") .style.width="0px";
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
